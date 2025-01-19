7d027744a7b77f1eb516fd164575a2332db523e7
Guardianprime <okongordian@gmail.com>
2025-01-19 20:51:24 +0100

    working version

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const main = document.getElementById("main");
 const successMessage = document.getElementById("success-message");
 const successEmailSpanEl = document.getElementById("success-email");
 const emailInput = document.getElementById("emailAdd");
-const errorMessage = document.getElementById("error-message");
+const errorMessage = document.getElementById("error-msg");
 
 /* Functions */
 
 const validateEmail = (email) => {
-    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
-    return emailRegex.test(email)
+    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    return emailRegex.test(email);
 }
 
 function handleSubmit(e) {
@@ -22,12 +22,11 @@ function handleSubmit(e) {
         successMessage.classList.toggle("hide");
         emailInput.classList.remove("invalid");
         errorMessage.classList.add("hide");
-
+        successEmailSpanEl.innerText = userEmail;
     } else {
         emailInput.classList.add("invalid");
         errorMessage.classList.remove("hide");
     }
-
 }
 
 function dissmis() {
@@ -35,7 +34,5 @@ function dissmis() {
     successMessage.classList.toggle("hide");
 }
 
-
-form.addEventListener("submit", handleSubmit)
-
+form.addEventListener("submit", handleSubmit);
 dismissBtn.addEventListener("click", dissmis);
